15fc11d9b3bafee5ad6f3d514a01aa2eec323ca7
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2021-01-01 15:22:13 +0900

    version 0.7.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ URL="https://github.com/KoichiYasuoka/spaCy-SynCha"
 
 setuptools.setup(
   name="spacy_syncha",
-  version="0.7.5",
+  version="0.7.6",
   description="SynCha-CaboCha-MeCab wrapper for spaCy",
   long_description=long_description,
   long_description_content_type="text/markdown",
